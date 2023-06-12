55ce9745ae85a0b5d332072c860bcc387b01335c
Richard Schilling <richard@aesirmachina.com>
2023-06-12 16:08:24 -0700

    Remove unused class XSLMessagres and remove unused function from XMLEventFactory.

--- android/androidXmlLib/src/main/java/android/xml/stream/XMLEventFactory.java
+++ android/androidXmlLib/src/main/java/android/xml/stream/XMLEventFactory.java
@@ -65,26 +65,13 @@ public abstract class XMLEventFactory {
 
     /**
      * Protected constructor to prevent instantiation.
-     * Use {@link #newFactory()} instead.
+     * Use {@link #newInstance()} instead.
      */
     protected XMLEventFactory(){}
 
-   /**
-   * Creates a new instance of the {@code XMLEventFactory} builtin
-   * system-default implementation.
-   *
-   * @return A new instance of the {@code XMLEventFactory} builtin
-   *         system-default implementation.
-   *
-   * @since 9
-   */
-  public static XMLEventFactory newDefaultFactory() {
-      return new XMLEventFactoryImpl();
-  }
 
   /**
-   * Creates a new instance of the factory in exactly the same manner as the
-   * {@link #newFactory()} method.
+   * Creates a new instance of the factory.
    *
    * @return an instance of the {@code XMLEventFactory}
    * @throws FactoryConfigurationError if an instance of this factory cannot be loaded
@@ -92,127 +79,13 @@ public abstract class XMLEventFactory {
   public static XMLEventFactory newInstance()
     throws FactoryConfigurationError
   {
-    return FactoryFinder.find(XMLEventFactory.class, DEFAULIMPL);
+      return new XMLEventFactoryImpl();
   }
 
-  /**
-   * Creates a new instance of the factory. This method uses the
-   * <a href="../../../module-summary.html#LookupMechanism">JAXP Lookup Mechanism</a>
-   * to determine the {@code XMLEventFactory} implementation class to load.
-   * <p>
-   * Once an application has obtained a reference to a {@code XMLEventFactory}, it
-   * can use the factory to configure and obtain stream instances.
-   *
-   * @return an instance of the {@code XMLEventFactory}
-   * @throws FactoryConfigurationError in case of {@linkplain
-   *   java.util.ServiceConfigurationError service configuration error} or if
-   *   the implementation is not available or cannot be instantiated.
-   */
-  public static XMLEventFactory newFactory()
-    throws FactoryConfigurationError
-  {
-    return FactoryFinder.find(XMLEventFactory.class, DEFAULIMPL);
-  }
 
-  /**
-   * Create a new instance of the factory
-   *
-   * @param factoryId             Name of the factory to find, same as
-   *                              a property name
-   * @param classLoader           classLoader to use
-   * @return the factory implementation
-   * @throws FactoryConfigurationError if an instance of this factory cannot be loaded
-   *
-   * @deprecated  This method has been deprecated to maintain API consistency.
-   *              All newInstance methods have been replaced with corresponding
-   *              newFactory methods. The replacement {@link
-   *              #newFactory(java.lang.String, java.lang.ClassLoader)}
-   *              method defines no changes in behavior.
-   */
-  @Deprecated(since="1.7")
-  public static XMLEventFactory newInstance(String factoryId,
-          ClassLoader classLoader)
-          throws FactoryConfigurationError {
-      //do not fallback if given classloader can't find the class, throw exception
-      return FactoryFinder.find(XMLEventFactory.class, factoryId, classLoader, null);
-  }
 
-  /**
-   * Create a new instance of the factory.
-   * If the classLoader argument is null, then the ContextClassLoader is used.
-   * <p>
-   * This method uses the following ordered lookup procedure to determine
-   * the XMLEventFactory implementation class to load:
-   * <ul>
-   * <li>
-   *   Use the value of the system property identified by {@code factoryId}.
-   * </li>
-   * <li>
-   *   <p>
-   *   Use the configuration file "stax.properties". The file is in standard
-   *   {@link java.util.Properties} format and typically located in the
-   *   conf directory of the Java installation. It contains the fully qualified
-   *   name of the implementation class with the key being the system property
-   *   defined above.
-   *
-   *   <p>
-   *   The stax.properties file is read only once by the implementation
-   *   and its values are then cached for future use.  If the file does not exist
-   *   when the first attempt is made to read from it, no further attempts are
-   *   made to check for its existence.  It is not possible to change the value
-   *   of any property in stax.properties after it has been read for the first time.
-   *
-   *   <p>
-   *   Use the jaxp configuration file "jaxp.properties". The file is in the same
-   *   format as stax.properties and will only be read if stax.properties does
-   *   not exist.
-   * </li>
-   * <li>
-   *   <p>
-   *   If {@code factoryId} is "android.xml.stream.XMLEventFactory",
-   *   use the service-provider loading facility, defined by the
-   *   {@link java.util.ServiceLoader} class, to attempt to {@linkplain
-   *   java.util.ServiceLoader#load(java.lang.Class, java.lang.ClassLoader) locate and load}
-   *   an implementation of the service using the specified {@code ClassLoader}.
-   *   If {@code classLoader} is null, the {@linkplain
-   *   java.util.ServiceLoader#load(java.lang.Class) default loading mechanism} will apply:
-   *   That is, the service-provider loading facility will use the {@linkplain
-   *   java.lang.Thread#getContextClassLoader() current thread's context class loader}
-   *   to attempt to load the service. If the context class
-   *   loader is null, the {@linkplain
-   *   ClassLoader#getSystemClassLoader() system class loader} will be used.
-   * </li>
-   * <li>
-   *   <p>
-   *   Otherwise, throws a {@link FactoryConfigurationError}.
-   * </li>
-   * </ul>
-   *
-   * <p>
-   * Note that this is a new method that replaces the deprecated
-   *   {@link #newInstance(java.lang.String, java.lang.ClassLoader)
-   *   newInstance(String factoryId, ClassLoader classLoader)} method.
-   * No changes in behavior are defined by this replacement method relative
-   * to the deprecated method.
-   *
-   * @apiNote The parameter factoryId defined here is inconsistent with that
-   * of other JAXP factories where the first parameter is fully qualified
-   * factory class name that provides implementation of the factory.
-   *
-   * @param factoryId             Name of the factory to find, same as
-   *                              a property name
-   * @param classLoader           classLoader to use
-   * @return the factory implementation
-   * @throws FactoryConfigurationError in case of {@linkplain
-   *   java.util.ServiceConfigurationError service configuration error} or if
-   *   the implementation is not available or cannot be instantiated.
-   */
-  public static XMLEventFactory newFactory(String factoryId,
-                                           ClassLoader classLoader)
-          throws FactoryConfigurationError {
-      //do not fallback if given classloader can't find the class, throw exception
-      return FactoryFinder.find(XMLEventFactory.class, factoryId, classLoader, null);
-  }
+
+
 
  /**
    * This method allows setting of the Location on each event that
